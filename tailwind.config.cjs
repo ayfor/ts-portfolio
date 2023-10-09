3912ddd02672ac582ab76f4cca7897d2e1f519f1
ayfor <josh.a.stubbington@gmail.com>
2023-10-09 01:06:15 -0400

    prettier config starting main page content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
-const typographyPlugin = require("@tailwindcss/typography");
-
+import typographyPlugin from '@tailwindcss/typography'
 
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	darkMode: "class",
 	plugins: [typographyPlugin],
+	theme: {
+		colors: {
+			current: 'currentColor',
+			'off-white': '#FDFFFC',
+			'off-white-transparent': 'rgba(253, 255, 252, 0.2)',
+			'off-black': '#040403',
+			'off-black-transparent': 'rgba(4, 4, 3, 0.60)',
+			'blue-highlight': '#45CAFF',
+			'purple-highlight': '#826AED',
+			'red-highlight': '#C42021',
+		},
+		extend: {
+			spacing: {
+				'0i': '0px !important',
+			}
+		}
+	}
 };
