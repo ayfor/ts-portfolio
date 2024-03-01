524abcd8e32efd78cb22a76e40f2d1c75a4be111
ayfor <josh.a.stubbington@gmail.com>
2024-03-01 00:56:57 -0500

    fixed project section sidebar coloring, sidebar icon colors now work when loading page to any scroll point, constants for sidebar section cutoffs

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,3 +1,8 @@
+const HOME_SECTION_CUTOFF = 500;
+const PROJECTS_SECTION_CUTOFF = 2000;
+const ABOUT_SECTION_CUTOFF = 3000;
+
+
 const homeIcon = document.querySelector('#home-icon svg');
 const projectsIcon = document.querySelector('#projects-icon svg');
 const aboutIcon = document.querySelector('#about-icon svg');
@@ -20,8 +25,12 @@ const activateIcon = (icon) =>  {
     if (key === 'projects') {
       if(icon === 'projects'){
         value.classList.add('text-blue-highlight');
+
+        value.classList.remove('text-off-white');
       } else {
         value.classList.remove('text-blue-highlight');
+        
+        value.classList.add('text-off-white');
       }
       return;
     }
@@ -34,20 +43,27 @@ const activateIcon = (icon) =>  {
   });
 }
 
-document.addEventListener('scroll', () => {
+const setIconDisplay = () => {
   const scrollPosition = Number(window.scrollY);
   switch (true) {
-    case (scrollPosition < 500):
+    case (scrollPosition < HOME_SECTION_CUTOFF):
       activateIcon('home');
       break;
-    case (scrollPosition >= 500 && scrollPosition < 2000):
+    case (scrollPosition >= HOME_SECTION_CUTOFF && scrollPosition < PROJECTS_SECTION_CUTOFF):
       activateIcon('projects');
       break;
-    case (scrollPosition >= 2000 && scrollPosition < 3000):
+    case (scrollPosition >= PROJECTS_SECTION_CUTOFF && scrollPosition < ABOUT_SECTION_CUTOFF):
       activateIcon('about');
       break;
-    case (scrollPosition >= 3000):
+    case (scrollPosition >= ABOUT_SECTION_CUTOFF):
       activateIcon('contact');
       break;
   }
+}
+
+document.addEventListener('scroll', () => {
+  setIconDisplay();
 });
+
+
+setIconDisplay();
