11c36e969f9ee0976df031b1dda6146550e22c2a
ayfor <josh.a.stubbington@gmail.com>
2024-03-09 01:04:19 -0500

    mobile menu hide and show now works with icon swap, corrected project section title on small screens

---
+++ src/scripts/mobile-sidebar.js
@@ -0,0 +1,31 @@
+// Get the side-menu-button element
+const sideMenuButton = document.getElementById('side-menu-button');
+
+// Get the mobile-menu element
+const mobileMenu = document.getElementById('mobile-menu');
+
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const closeIcon = document.getElementById('close-icon');
+
+// Add click event listener to the side-menu-button
+sideMenuButton.addEventListener('click', () => {
+  // Toggle the visibility of the mobile-menu element
+  mobileMenu.classList.toggle('hidden');
+
+  // Swap the icon in the side-menu-button
+  if (mobileMenu.classList.contains('hidden')) {
+    // If the mobile-menu is hidden, show the hamburger icon
+    hamburgerIcon.classList.remove('hidden');
+    hamburgerIcon.classList.add('block');
+
+    closeIcon.classList.add('hidden');
+    closeIcon.classList.remove('block');
+  } else {
+    // If the mobile-menu is shown, show the close icon
+    hamburgerIcon.classList.add('hidden');
+    hamburgerIcon.classList.remove('block');
+
+    closeIcon.classList.remove('hidden');
+    closeIcon.classList.add('block');
+  }
+});
