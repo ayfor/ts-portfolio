ad25395110e4852696d5e5416bbb3fd291e7304c
ayfor <josh.a.stubbington@gmail.com>
2024-03-27 23:36:45 -0400

    project card tile body opacity tweak

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,9 +23,6 @@ module.exports = {
 			'transparent': 'transparent',
 		},
 		extend: {
-			animate: {
-				'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-			},
 			margin: {
 				22: '5.5rem',
 			},
