575863a493edb151ee7a73bd67768c763c970e7d
ayfor <josh.a.stubbington@gmail.com>
2024-03-27 22:57:31 -0400

    blurred animated bg spruce assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,6 +23,9 @@ module.exports = {
 			'transparent': 'transparent',
 		},
 		extend: {
+			animate: {
+				'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+			},
 			margin: {
 				22: '5.5rem',
 			},
